ec649eb2ee22fb8329b1242ae0615181646264e5
Hadrien Grasland <guydeloinbard@yahoo.fr>
2010-11-21 19:51:56 +0000

    Forgot one file

---
+++ include/kmaths.h
@@ -0,0 +1,25 @@
+ /* Basic math macros
+
+      Copyright (C) 2010  Hadrien Grasland
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA */
+
+#ifndef _KMATHS_H_
+#define _KMATHS_H_
+
+#define max(op1, op2) (((op1)>=(op2))?(op1):(op2))
+#define min(op1, op2) (((op1)<=(op2))?(op1):(op2))
+
+#endif
