90a3a55460b274d56d72ec61395393fadd1cca1b
Hadrien G <knights_of_ni@gmx.com>
2013-01-14 08:01:44 +0100

    Defined the interface to UTF-x strings

--- kernel/include/kstring.h
+++ kernel/include/kstring.h
@@ -1,6 +1,6 @@
  /* Equivalent of cstring (aka string.h), but not a full implementation. Manipulates chunks of RAM,
     and maybe C-style strings and array in the future.
-    Also includes definition of the KString object, a high-level string container used for
+    Also includes definition of the KAsciiString object, a high-level string container used for
     communication between the kernel and processes.
 
       Copyright (C) 2011  Hadrien Grasland
@@ -27,19 +27,21 @@
 void* memcpy(void* destination, const void* source, size_t num);
 size_t strlen(const char* str);
 
-class KString {
+typedef class KAsciiString KString;
+
+class KAsciiString { //TO BE REMOVED
   private:
     size_t len;
     char* contents;
     size_t current_location;
   public:
     //Initialization and destruction
-    KString(const char* source = "");
-    KString(const KString& source);
-    ~KString() {clear();}
-    KString& operator=(const char* source);
-    KString& operator=(const KString& source);
-    void clear(); //Resets a KString to a blank state
+    KAsciiString(const char* source = "");
+    KAsciiString(const KAsciiString& source);
+    ~KAsciiString() {clear();}
+    KAsciiString& operator=(const char* source);
+    KAsciiString& operator=(const KAsciiString& source);
+    void clear(); //Resets a KAsciiString to a blank state
 
     //Length of the string in ASCII chars
     size_t length() const {return len;}
@@ -47,31 +49,110 @@ class KString {
 
     //Comparison
     bool operator==(const char* param) const;
-    bool operator==(const KString& param) const;
+    bool operator==(const KAsciiString& param) const;
     bool operator!=(const char* param) const {return !operator==(param);}
-    bool operator!=(const KString& param) const {return !operator==(param);}
+    bool operator!=(const KAsciiString& param) const {return !operator==(param);}
 
     //Concatenation
-    KString& operator+=(const char* source);
-    KString& operator+=(const KString& source);
+    KAsciiString& operator+=(const char* source);
+    KAsciiString& operator+=(const KAsciiString& source);
     //No operator+ because it cannot be implemented with good performance in C++
 
     //Extract of a subset of another string into this string
-    bool extract_from(KString& source, size_t first_char, size_t length);
+    bool extract_from(KAsciiString& source, size_t first_char, size_t length);
 
     //Paste a string, or a portion of it, in the middle of another, erasing existing characters
-    bool paste(KString& source, size_t dest_index) {return paste(source, 0, source.length(), dest_index);}
-    bool paste(KString& source, size_t first_char, size_t length, size_t dest_index);
+    bool paste(KAsciiString& source, size_t dest_index) {return paste(source, 0, source.length(), dest_index);}
+    bool paste(KAsciiString& source, size_t first_char, size_t length, size_t dest_index);
 
     //Indexed character access
     char& operator[](const uint32_t index) const {return contents[index];}
 
     //Text file parsing
-    bool read_line(KString& dest); //Returns false if at end of file, true otherwise.
+    bool read_line(KAsciiString& dest); //Returns false if at end of file, true otherwise.
     size_t line_index() {return current_location;}
     void goto_index(size_t index) {current_location = index;}
 
     //Returns the size of the heap object associated to this string
+    size_t heap_size();
+};
+
+typedef class KUTF32String KUTF32String;
+typedef class KUTF8String KUTF8String;
+
+class KUTF32String {
+  private:
+    size_t len;
+    uint32_t* contents;
+    size_t current_location;
+  public:
+    //Initialization and destruction
+    KUTF32String(const KUTF32String& source);
+    KUTF32String(const KUTF8String& source);
+    KUTF32String(const char32_t* source);
+    KUTF32String(const char* source = ""); //For use with ASCII strings only (Is it possible to programmatically differentiate those ?)
+    KUTF32String& operator=(const KUTF32String& source);
+    KUTF32String& operator=(const KUTF8String& source);
+    KUTF32String& operator=(const char32_t* source);
+    KUTF32String& operator=(const char* source); //For use with ASCII strings only
+    ~KUTF32String() {clear();}
+    void clear(); //Resets a KUTF32String to a blank state
+
+    //Length of the string in Unicode code points
+    size_t length() const {return len;}
+    void set_length(size_t desired_length, bool keep_contents = true);
+
+    //String comparison
+    bool operator==(const KUTF32String& param) const;
+    bool operator!=(const KUTF32String& param) const {return !operator==(param);}
+
+    //String concatenation
+    KUTF32String& operator+=(const KUTF32String& source);
+    //No operator+ because it cannot be implemented efficiently in C++
+
+    //Extract of a subset of another string into this string
+    bool extract_from(KUTF32String& source, size_t first_char, size_t length);
+
+    //Paste a string, or a portion of it, in the middle of another, erasing existing characters.
+    //Leaving length to zero means that the whole source string must be pasted.
+    //WARNING : This is semantically different from KAsciiString's paste() !
+    bool paste(KUTF32String& source, size_t dest_index, size_t first_char = 0, size_t length = 0);
+
+    //Indexed character access
+    uint32_t& operator[](const size_t index) const {return contents[index];}
+
+    //Text file parsing
+    bool read_line(KUTF32String& dest); //Returns false if at end of file, true otherwise.
+    size_t line_index() {return current_location;}
+    void seek_index(size_t index) {current_location = index;}
+
+    //Returns the size of the heap objects associated to this string
+    size_t heap_size();
+};
+
+class KUTF8String {
+  private:
+    size_t len;
+    uint8_t* contents;
+  public:
+    //Initialization and destruction
+    KUTF8String(const KUTF8String& source);
+    KUTF8String(const KUTF32String& source);
+    KUTF8String(const char* source = ""); //For use with UTF-8 strings only (Is it possible to programmatically differentiate those ?)
+    KUTF8String& operator=(const KUTF8String& source);
+    KUTF8String& operator=(const KUTF32String& source);
+    KUTF8String& operator=(const char* source); //For use with UTF-8 strings only
+    ~KUTF8String() {clear();}
+    void clear(); //Resets a KUTF8String to a blank state
+    
+    //Length of the string in bytes (which is NOT the same as code points)
+    size_t length() const {return len;}
+    void set_length(size_t desired_length, bool keep_contents = true);
+    
+    //Individual byte access
+    uint8_t& operator[](const size_t index) const {return contents[index];}
+    
+    //Returns the size of the heap objects associated to this string
     size_t heap_size();
 };
 
