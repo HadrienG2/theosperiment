7e1df31551dd9a5a916d6f6c5dfae71eb15cfa9e
Hadrien G <knights_of_ni@gmx.com>
2013-04-02 19:03:19 +0200

    Moving logo display to a separate bootstrap function

--- bootstrap/arch/i686/bs_main.c
+++ bootstrap/arch/i686/bs_main.c
@@ -1,6 +1,6 @@
 /* The bootstrap kernel, whose goal is to load the actual kernel
 
-    Copyright (C) 2010    Hadrien Grasland
+    Copyright (C) 2010-2013    Hadrien Grasland
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@
 #include <asm_routines.h>
 #include <bs_KernelInformation.h>
 #include <die.h>
+#include <print_logo.h>
 #include <sgt_generation.h>
 #include <stdint.h>
 #include <kernel_loader.h>
@@ -26,37 +27,6 @@
 #include <multiboot.h>
 #include <paging.h>
 #include <txt_videomem.h>
-
-void print_logo() {
-    //A TOSP logo, made in ASCII art. It is 78x13 characters large
-    //
-    //o   o
-    print_chr(-36);  movecur_rel(+3,0);  print_chr(-36); movecur_rel(-5,+1);
-    //O   O
-    print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(-5,+1);
-    //O°° O°°o o°°o o°°°°°°o o°°°°°°° O                                          0
-    print_chr(-37);  print_chr(-33);  print_chr(-33);  movecur_rel(+1,0);  print_chr(-37);  print_chr(-33);  print_chr(-33);  print_chr(-36);  movecur_rel(+1,0);  print_chr(-36);  print_chr(-33);  print_chr(-33);  print_chr(-36);  movecur_rel(+1,0);  print_chr(-36);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-36);  movecur_rel(+1,0);  print_chr(-36);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+42,0);  print_chr(-37);  movecur_rel(-76,+1);
-    //O   O  O OooO O      O O        O                                          0
-    print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+2,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  print_chr(-36);  print_chr(-36);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+6,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+8,0);  print_chr(-37);  movecur_rel(+42,0);  print_chr(-37);  movecur_rel(-76,+1);
-    //O   O  O O    O      O O        O                  o                       0
-    print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+2,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+4,0);  print_chr(-37);  movecur_rel(+6,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+8,0);  print_chr(-37);  movecur_rel(+18,0); print_chr(-33); movecur_rel(+23,0);  print_chr(-37);  movecur_rel(-76,+1);
-    // °° °  °  °°° O      O  °°°°°°o O 0°°°o o°°°o o°°° O O°°°O°°°o o°°°o O°°°o 0°°°
-    movecur_rel(+1,0);  print_chr(-33);  print_chr(-33);  movecur_rel(+1,0);  print_chr(-33);  movecur_rel(+2,0);  print_chr(-33);  movecur_rel(+2,0);  print_chr(-33);  print_chr(-33);  print_chr(-33);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+6,0);  print_chr(-37);  movecur_rel(+2,0);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-36);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-36);  movecur_rel(+1,0);  print_chr(-36);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-36);  movecur_rel(+1,0);  print_chr(-36);  print_chr(-33);  print_chr(-33);  print_chr(-33);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-37);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-36);  movecur_rel(+1,0);  print_chr(-36);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-36);  movecur_rel(+1,0);  print_chr(-37);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-36);  movecur_rel(+1,0);  print_chr(-37);  print_chr(-33);  print_chr(-33);  movecur_rel(-78,+1);
-    //              0      0        0 0 0   0 0   0 0    0 0   0   0 0   0 0   0 0
-    movecur_rel(+14,0);  print_chr(-37);  movecur_rel(+6,0);  print_chr(-37);  movecur_rel(+8,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+4,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(-76,+1);
-    //              0      0        0 0 0   0 0°°°° 0    0 0   0   0 0°°°° 0   0 0
-    movecur_rel(+14,0);  print_chr(-37);  movecur_rel(+6,0);  print_chr(-37);  movecur_rel(+8,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+4,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(-76,+1);
-    //              0      0        0 0 0   0 0     0    0 0   0   0 0     0   0 0
-    movecur_rel(+14,0);  print_chr(-37);  movecur_rel(+6,0);  print_chr(-37);  movecur_rel(+8,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+5,0);  print_chr(-37);  movecur_rel(+4,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(+5,0);  print_chr(-37);  movecur_rel(+3,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(-76,+1);
-    //               °°°°°°  °°°°°°°  0 O°°°   °°°° °    ° °   °   °  °°°° °   °  °°
-    movecur_rel(+15,0);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  movecur_rel(+2,0);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  movecur_rel(+2,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  print_chr(-33);  print_chr(-33);  print_chr(-33);  movecur_rel(+3,0);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  movecur_rel(+1,0);  print_chr(-33);  movecur_rel(+4,0);  print_chr(-33);  movecur_rel(+1,0);  print_chr(-33);  movecur_rel(+3,0);  print_chr(-33);  movecur_rel(+3,0);  print_chr(-33);  movecur_rel(+2,0);  print_chr(-33);  print_chr(-33);  print_chr(-33);  print_chr(-33);  movecur_rel(+1,0);  print_chr(-33);  movecur_rel(+3,0);  print_chr(-33);  movecur_rel(+2,0);  print_chr(-33);  print_chr(-33);  movecur_rel(-78,+1);
-    //                                0 O
-    movecur_rel(+32,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(-35,+1);
-    //                                0 O
-    movecur_rel(+32,0);  print_chr(-37);  movecur_rel(+1,0);  print_chr(-37);  movecur_rel(-35,+1);
-    //                                ° °
-    movecur_rel(+32,0);  print_chr(-33);  movecur_rel(+1,0);  print_chr(-33);  print_chr('\n');
-}
 
 int bootstrap_longmode(const multiboot_info_t* mbd, const uint32_t magic) {
     KernelInformation* kinfo;
